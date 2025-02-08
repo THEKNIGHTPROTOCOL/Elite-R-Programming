e80ddf0a97f6d98d6c2eed7166cc87c671d6fba3
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-02-08 20:37:39 +0530

    Update

---
+++ Transpose of a Matrix Using the t() Function.R
@@ -0,0 +1,9 @@
+# Creating a matrix (3 rows, 2 columns)
+A <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2)
+print("Original Matrix:")
+print(A)
+
+# Transpose of the matrix
+A_transpose <- t(A)
+print("Transposed Matrix:")
+print(A_transpose)
