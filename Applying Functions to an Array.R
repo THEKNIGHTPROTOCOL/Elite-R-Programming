52e9ccc880cea92cfc3cb33edfef420c85a87109
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-02-14 17:09:08 +0530

    Update

---
+++ Applying Functions to an Array.R
@@ -0,0 +1,7 @@
+# Finding sum of elements along rows (MARGIN=1)
+row_sums <- apply(arr, 1, sum)
+print(row_sums)
+
+# Finding sum of elements along columns (MARGIN=2)
+col_sums <- apply(arr, 2, sum)
+print(col_sums)
