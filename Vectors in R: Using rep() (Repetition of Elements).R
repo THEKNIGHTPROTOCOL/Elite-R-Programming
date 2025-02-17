0d05331496f532ad4a87ece48c9517c593e00380
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-02-17 20:29:01 +0530

    Update

---
+++ Vectors in R: Using rep() (Repetition of Elements).R
@@ -0,0 +1,5 @@
+num_vec <- c(1, 2, 3, 4, 5)  # Numeric vector
+char_vec <- c("A", "B", "C")  # Character vector
+log_vec <- c(TRUE, FALSE, TRUE)  # Logical vector
+
+rep_vec <- rep(1:3, times = 2)  # Repeats 1,2,3 two times: 1 2 3 1 2 3
