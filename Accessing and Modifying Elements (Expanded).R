0d9399eae9ee38fa02f938509f1ab13b8577bf17
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-16 08:15:24 +0530

    Update Accessing and Modifying Elements (Expanded).R

--- Accessing and Modifying Elements (Expanded).R
+++ Accessing and Modifying Elements (Expanded).R
@@ -2,7 +2,7 @@
 my_array[1, 2, 3]  # Element at Row1, Col2, L3
 my_array["Row1", "Col2", "L3"] # Using dimension names
 
-# Slicing:
+# Slicing: 
 my_array[1, , ]      # All elements in Row1 across all columns and layers
 my_array[, "Col2", 1:2]  # All elements in Col2 of layers L1 and L2
 my_array[1:2, 2:3, c("L1", "L3")] # A sub-array
